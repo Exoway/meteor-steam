15af1d83b3f07ebee97d7142c811c8324503468a
Gaëtan Dezeiraud <gaetan.dezeiraud@outlook.com>
2020-03-25 16:21:38 +0100

    Fix versionsFrom

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.0.0');
+  api.versionsFrom('1.2.0.2');
 
   api.use('oauth2', ['client', 'server']);
   api.use('oauth', ['client', 'server']);
